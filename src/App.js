891642fed1e50c11ae202bf35fc45df593477e20
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-11-22 15:13:02 -0500

    Rendering working so far!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
+import React, { useState } from "react";
+
 import "./App.css";
+// Header
+import Header from "./components/Header";
+// Navigation
+import Navigation from "./components/Navigation";
 // About || Home
 import About from "./pages/About";
-// Portfolio > Projects
+//
 import Portfolio from "./pages/Portfolio";
-// Contact
+//
 import Contact from "./pages/Contact";
-// Resume
+//
 import Resume from "./pages/Resume";
+// Footer
+import Footer from "./components/Footer";
 
 // Use conditional rendering?
 // map function
 
 function App() {
-  return <div className="App"></div>;
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+  if (currentPage === "Resume") {
+    return <Resume />;
+  }
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      <Header />
+      {/* We are passing the currentPage from state and the function to update it */}
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
