99b9398823824455cb47075e0da374705cce2efe
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-11-22 14:00:18 -0500

    structuring

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import './App.css';
-
-// About
-// Projects
+import "./App.css";
+// About || Home
+import About from "./pages/About";
+// Portfolio > Projects
+import Portfolio from "./pages/Portfolio";
 // Contact
+import Contact from "./pages/Contact";
 // Resume
+import Resume from "./pages/Resume";
 
 // Use conditional rendering?
+// map function
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
