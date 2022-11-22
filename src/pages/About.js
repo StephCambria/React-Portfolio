e7dd61284f7de186d64071602b0d337442a1fd8a
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-11-22 14:23:23 -0500

    referencing classwork

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import Navigation from "../components/Navigation";
+import Portfolio from "./Portfolio";
+import Contact from "./Contact";
+import Resume from "./Resume";
 
 // About will be the home page
 // containing a brief introduction and a short summary of skills
 
 function About() {
-  return <div>About</div>;
+  const [currentPage, setCurrentPage] = useState("Home");
+
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+  if (currentPage === "Resume") {
+    return <Resume />;
+  }
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* We are passing the currentPage from state and the function to update it */}
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+      {/* Here we are calling the renderPage method which will return a component  */}
+      {renderPage()}
+      <div>
+        <h1>About</h1>
+        <p>
+          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus
+          cursus in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit
+          at.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default About;
