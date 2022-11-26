3cd90a5e9c17569df573b22e2daf359ef5e23ac8
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-11-26 18:10:50 -0500

    portfolio framework

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,10 +1,22 @@
 import React from "react";
+import Project from "../components/Project";
+import "../styles/Portfolio.css";
 
 // Portfolio will be the page containing images of all projects.
 // Individual projects will be contained in either components or a seperate folder.
 
 function Portfolio() {
-  return <div></div>;
+  return <div className="portfolio">
+    <h3 className="title">Portfolio</h3>
+    <div className="projectsList">
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+      <Project />
+    </div>
+  </div>;
 }
 
 export default Portfolio;
