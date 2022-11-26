c68dc609c90d40f74d9b9d5df0173e0d7be0e4f6
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-11-26 17:52:17 -0500

    contact form done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,37 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Contact.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { validateEmail } from "../utils/helpers";
 
 // Contact will contain a simple form
-// Needs event listeners
-// Needs mailto
 
 function Contact() {
+  const [email, setEmail] = useState("");
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "email") {
+      setEmail(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setErrorMessage("Not a valid email");
+      return;
+    }
+  };
+  setEmail("");
+
   return (
     <div className="contactBody">
       <div>
-        <label for="exampleFormControlInput1" class="form-label">
+        <label for="exampleFormControlInput1" className="form-label">
           Email Me!
         </label>
         <input
           type="email"
-          class="form-control"
+          value={email}
+          onChange={handleInputChange}
+          className="form-control"
           id="exampleFormControlInput1"
           placeholder="name@example.com"
+          Mailto="steph.cambria.art@gmail.com"
         />
       </div>
       <div>
-        <label for="exampleFormControlTextarea1" class="form-label">
-          
-        </label>
+        <label for="exampleFormControlTextarea1" className="form-label"></label>
         <textarea
-          class="form-control"
+          className="form-control"
           id="exampleFormControlTextarea1"
           placeholder="Questions, comments, concerns..."
           rows={5}
         ></textarea>
       </div>
-      <button className="sendBtn">Send</button>
+      <button className="sendBtn" onClick={handleFormSubmit}>
+        Send
+      </button>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 }
