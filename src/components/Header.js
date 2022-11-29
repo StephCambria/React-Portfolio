9829e465650401fe13dd22e6020ba10306ec79e7
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-11-29 16:35:10 -0500

    keeping up to date

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,13 @@ import React from "react";
 import "../styles/Header.css";
 
 // Header will be the heading that appears on each page
-// Planning to add either CSS typography of my name,
-// or something hand-made with it.
 
 function Header() {
   return (
     <div className="header">
       <img
         className="headerphoto"
-        src={require("../images/header10.jpg")}
+        src={require("../images/cover4.png")}
         alt={""}
       />
     </div>
