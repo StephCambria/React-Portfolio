7bb8b35d581f64d37b86be955d6fc59ceeaf7dac
Stephanie Cambria <107421370+StephCambria@users.noreply.github.com>
2022-11-30 11:12:15 -0500

    working

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Contact.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-//import { validateEmail } from "../utils/helpers";
+import { validateEmail } from "../utils/helpers";
 
 // Contact will contain a simple form
 
 function Contact() {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "userName") {
+      setUserName(inputValue);
+    } else if (inputType === "email") {
+      setEmail(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email) || !userName) {
+      setErrorMessage("Invalid email or username");
+      return;
+    }
+
+    setEmail("");
+    setUserName("");
+  };
+
   return (
     <div className="contactBody">
       <div>
         <label for="exampleFormControlInput1" className="form-label">
           Email Me!
         </label>
-        <input type="name" className="form-control" placeholder="name" />
+        <input
+          value={userName}
+          name="userName"
+          onChange={handleInputChange}
+          type="text"
+          className="form-control"
+          placeholder="name"
+        />
         <br></br>
         <input
-          type="email"
+          name="email"
+          onChange={handleInputChange}
           className="form-control"
           id="exampleFormControlInput1"
           placeholder="name@example.com"
@@ -32,7 +68,14 @@ function Contact() {
         ></textarea>
       </div>
       <br></br>
-      <button className="sendBtn">Send</button>
+      <button className="sendBtn" onClick={handleFormSubmit}>
+        Send
+      </button>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
 
       <div className="contactText">
         <br></br>
